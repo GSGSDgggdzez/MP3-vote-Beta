7113709e7746dddd9fe3929c4aeb3ccbea5c3d7c
GSGSDgggdzez <haroddefree6@gmail.com>
2025-03-02 18:40:04 +0100

    OK

--- vote-cameroon-beta/src/app/components/Fotter.tsx
+++ vote-cameroon-beta/src/app/components/Fotter.tsx
@@ -4,8 +4,6 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-green-900 text-white">
-      
-        
         <div className="border-t border-gray-700 mt-6 pt-6 text-center">
           <p className="text-sm text-gray-400">
             © {new Date().getFullYear()} vote cameroon
